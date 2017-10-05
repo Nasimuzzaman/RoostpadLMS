4fa3216347cbb1fe4839ebc5e536a97952f528f4
Unknown <bsse0602@iit.du.ac.bd>
2017-10-05 17:10:14 +0600

    SQL DB

---
+++ model/db.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nasim
+ * Date: 05-Oct-17
+ * Time: 4:53 PM
+ */
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "mms";
+$con = mysqli_connect($host, $user, $password, $database) or die("error");
+
+?>
