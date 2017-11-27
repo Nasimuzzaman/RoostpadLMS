f181d16c03a8de1c187834daec904fb9a1b9af4c
Nasimuzzaman <bsse0602@iit.du.ac.bd>
2017-11-27 15:23:07 +0600

    secure pending leave request

--- controller/GetPendingLeaveRequests.php
+++ controller/GetPendingLeaveRequests.php
@@ -10,24 +10,43 @@ include_once '../model/config.php';
 
 if ($conn->connect_error == false) {
 
-    $sql = "SELECT leave_requests.*, users.name from leave_requests LEFT JOIN users ON leave_requests.email = users.email WHERE leave_requests.status = 'pending'";
+    $jsonBody = file_get_contents('php://input');
+    $params = json_decode($jsonBody);
+    $params = (array)$params;
+    $email = $params["email"];
+    $token = $params["token"];
+
+    $sqlInfo = ("SELECT token, role FROM users WHERE email = '$email'") or exit(mysqli_error());
+    $info = $conn->query($sqlInfo);
 
 
-    if($conn->query($sql)) {
+    $data = $info->fetch_assoc();
 
-        $result = $conn->query($sql);
-        $pendingRequests = array();
-        while (($row = $result->fetch_assoc()) != null) {
-            $pendingRequests[] = $row;
+    if($data["token"] == $token && $data["role"] == "CTO") {
+        $sql = "SELECT leave_requests.*, users.name from leave_requests LEFT JOIN users ON leave_requests.email = users.email WHERE leave_requests.status = 'pending'";
+
+        if($conn->query($sql)) {
+
+            $result = $conn->query($sql);
+            $pendingRequests = array();
+            while (($row = $result->fetch_assoc()) != null) {
+                $pendingRequests[] = $row;
+            }
+            $params["contactList"] = $pendingRequests;
+            $params["message"] = "";
+            $params["statusCode"] = 200;
+            $params["error"] = "";
+            echo json_encode($params);
+        } else {
+            printError( "Could not get pending requests! Try again!");
         }
-        $params["contactList"] = $pendingRequests;
-        $params["message"] = "";
-        $params["statusCode"] = 200;
-        $params["error"] = "";
-        echo json_encode($params);
     } else {
-        printError( "Could not get pending requests! Try again!");
+        $params["message"] = "Permission denied";
+        $params["statusCode"] = 403;
+        $params["error"] = "You don't have sufficient permission";
     }
+
+
 
     $conn->close();
 
